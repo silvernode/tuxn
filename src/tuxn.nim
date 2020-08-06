f850d39555db811a150fc470ae819a152d421b01
mollusk <silvernode@gmail.com>
2020-08-05 23:50:54 -0700

    add arg parsing

--- src/tuxn.nim
+++ src/tuxn.nim
@@ -1,7 +1,67 @@
 # This is just an example to get you started. A typical hybrid package
 # uses this file as the main entry point of the application.
 
-import tuxnpkg/submodule
+import 
+  tuxnpkg/package_managers,
+  tuxnpkg/procs,
+  os, 
+  strformat, 
+  strutils, 
+  tables
 
 when isMainModule:
-  echo(getWelcomeMessage())
+  var argCount = paramCount()
+  var cmdArgs = commandLineParams()
+  var pkgArgs: TaintedString
+  var pkm: PackageManager
+
+  if fileExists(apt.filePath):
+    pkm = apt
+  elif fileExists(dnf.filePath):
+    pkm = dnf
+  elif fileExists(eopkg.filePath):
+    pkm = eopkg
+  elif fileExists(pacman.filePath):
+    pkm = pacman
+  else:
+    echo "No supported package manager found"
+    quit(0)
+
+  
+
+
+  for n in 1 ..< argCount:
+    echo cmdArgs[n]
+    pkgArgs = pkgArgs & " " & cmdArgs[n]
+
+  case cmdArgs[0]
+
+    of "i", "-i","install":
+      pkmCmd(pkm.installCmd, pkgArgs)
+    
+    of "ri", "-ri", "reinstall":
+      pkmCmd(pkm.reinstallCmd, pkgArgs)
+
+    of "r", "-r", "remove":
+      pkmCmd(pkm.removeCmd, pkgArgs)
+
+    of "u", "-u", "upgrade":
+      pkmCmd(pkm.upgradeCmd, pkgArgs)
+
+    of "sup", "-sync-and-upgrade":
+      pkmCmd(pkm.supCmd, pkgArgs)
+
+    of "s", "-s", "search":
+      pkmCmd(pkm.searchCmd, pkgArgs)
+
+    of "c", "-c", "clean":
+      pkmCmd(pkm.cleanCmd, pkgArgs)
+
+    of "if", "-if", "info":
+      pkmCmd(pkm.infoCmd, pkgArgs)
+
+    else:
+      helpOpt()
+      quit(0)
+  
+
