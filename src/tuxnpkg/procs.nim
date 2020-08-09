c9928470c5ae621141cbbea31a6dd0ac1244cc63
mollusk <silvernode@gmail.com>
2020-08-08 21:38:35 -0700

    Add support for custom package manager

--- src/tuxnpkg/procs.nim
+++ src/tuxnpkg/procs.nim
@@ -1,20 +1,21 @@
 import
-  package_managers,
-  os, strformat
+  os, 
+  strformat
 
 proc helpOpt*() =
     echo """
-    tuxn [i, r, u, sup, s, c ] <pkgname>
 
-    i     install a package
-    ri    reinstall a package
-    r     remove a package
-    up    update system
-    ug    upgrade system
-    sup   sync and upgrade
-    s     search for a package
-    c     clean package cache
-    in    show info for package
+tuxn [i, r, u, sup, s, c ] <pkgname>
+
+i     install a package
+ri    reinstall a package
+r     remove a package
+up    update system
+ug    upgrade system
+sup   sync and upgrade
+s     search for a package
+c     clean package cache
+in    show info for package
     """
 
 proc pkmCmd*(command: string, arg: TaintedString) =
@@ -25,9 +26,13 @@ proc pkmCmd*(command: string, arg: TaintedString) =
       echo command, arg
       discard execShellCmd(fmt"{command} {arg}")
     
-
+proc sigTerm() {.noconv.} =
+      echo "cancelled"
+      quit(1)
+    
 
 proc customPkm*() =
+  setControlCHook(sigTerm)
   ## Interactively create custom package manager bindings
   stdout.write("Name of custom package manager: ")
   var pkmName = readLine(stdin)
@@ -38,11 +43,11 @@ proc customPkm*() =
   stdout.write("Install command: ")
   var pkmInstallCmd = readLine(stdin)
 
-  stdout.write("Reinstall command: ")
-  var pkmReinstallCmd = readLine(stdin)
-#[
   stdout.write("Remove command: ")
   var pkmRemoveCmd = readLine(stdin)
+
+  stdout.write("Reinstall command: ")
+  var pkmReinstallCmd = readLine(stdin)
 
   stdout.write("Update command: ")
   var pkmUpdateCmd = readLine(stdin)
@@ -50,7 +55,7 @@ proc customPkm*() =
   stdout.write("Upgrade command: ")
   var pkmUpgradeCmd = readLine(stdin)
 
-  stdout.write("Sync & Upgrade command (sup): ")
+  stdout.write("Sync & Update command: ")
   var pkmSupCmd = readLine(stdin)
 
   stdout.write("Search command: ")
@@ -61,15 +66,25 @@ proc customPkm*() =
 
   stdout.write("Info command: ")
   var pkmInfoCmd = readLine(stdin)
-]#
-  let lines = [pkmName, pkmFilePath, pkmInstallCmd, pkmReinstallCmd]
-  # echo "Writing file"
-  # writeFile(pkmName, pkmFilePath)
+
+  
+
+  let lines = [fmt"#{pkmName}", 
+  fmt"filePath = {pkmFilePath}", 
+  fmt"installCmd = {pkmInstallCmd}", 
+  fmt"removeCmd = {pkmRemoveCmd}",
+  fmt"reinstallCmd = {pkmReinstallCmd}", 
+  fmt"updateCmd = {pkmUpdateCmd}", 
+  fmt"upgradeCmd = {pkmUpgradeCmd}", 
+  fmt"supCmd = {pkmSupCmd}", 
+  fmt"searchCmd = {pkmSearchCmd}", 
+  fmt"cleanCmd = {pkmCleanCmd}", 
+  fmt"infoCmd = {pkmInfoCmd}"]
+
+  
   echo "Opening file"
   let file = open(pkmName, fmWrite)
- # echo "Reading first line"
- # echo file.readLine()
- # echo "Closing file"
+ 
   defer: file.close()
 
   for line in lines:
