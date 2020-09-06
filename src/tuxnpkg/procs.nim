430fdcf33aca2ea9e9ed1bdc7dbc8328a29eeb7a
mollusk <silvernode@gmail.com>
2020-09-05 21:42:55 -0700

    Add update cmd to usage string

--- src/tuxnpkg/procs.nim
+++ src/tuxnpkg/procs.nim
@@ -7,7 +7,7 @@ import
 proc helpOpt*() =
     echo """
 
-tuxn [i, r, u, sup, s, c, in, v ] <pkgname>
+tuxn [i, r, up, u, sup, s, c, in, v ] <pkgname>
 
 i     install a package
 ri    reinstall a package
